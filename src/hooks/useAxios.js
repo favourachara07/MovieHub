72bcd6d46305dee9ee09e8ea18ec5f1ddddd1628
favourachara07 <acharafavour01@gmail.com>
2024-09-17 00:05:24 +0100

    Loading feature

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,15 +1,27 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import axios from "axios";
-// converting useEffect into useAxios
-// axios: A promise-based HTTP client for making requests to APIs.
-export const useAxios = (apiPath, queryTerm="") => {
-    const [data, setData]=useState([])
-    const url=`https://api.themoviedb.org/3/${apiPath}?api_key=e9fe41fd5626415eb9b028cc98a24587&query=${queryTerm}`
-  console.log(apiPath)
-    useEffect(() => {
-        axios.get(url)
-            .then(response => setData(response.data.results))
-            .catch(error => console.error("Error fetching movies:", error));
-    }, [url]);
-  return { data }
-}
+
+// Custom hook to handle axios requests
+export const useAxios = (apiPath, queryTerm = "") => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=e9fe41fd5626415eb9b028cc98a24587&query=${queryTerm}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(url);
+        setData(response.data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading };
+};
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,20 +1,23 @@
 /* eslint-disable react/prop-types */
 import { Card } from "../components/Card";
+import Loader from "../components/Loader";
 import { useAxios, useTitle } from "../hooks";
 
 export const MovieList = ({ apiPath, title }) => {
     //  the below resembles something liek: const {id, title, overview, poster_path}=movie
     // useAxios("...") calls the custom hook, which fetches the data from the API and returns an object with a data property, { data: movies } destructures the object, taking the data property and renaming it to movies.
-    const { data: movies } = useAxios(apiPath)
+    const { data: movies, loading } = useAxios(apiPath)
     // page title
     useTitle(title)
     return (
         <main>
             <section className="max-w-7xl mx-auto py-7">
-                <div className="flex justify-start flex-wrap other:justify-evenly">
-                    {movies.map((movie) => (
+                <div className="flex justify-start flex-wrap sm:justify-evenly">
+                    {!loading ? movies.map((movie) => (
                         <Card key={movie.id} movie={movie} />
-                    ))}
+                    )) : 
+                    <Loader />
+                    }
                 </div>
             </section>
         </main>

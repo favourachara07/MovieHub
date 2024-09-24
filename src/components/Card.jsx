9f881a795c08e2adc8b67afc5c86490440ee0ce9
favourachara07 <acharafavour01@gmail.com>
2024-09-25 00:02:18 +0100

    Card updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,31 @@
-import { Link } from "react-router-dom"
-import CondImg from "../assets/geoffrey-moffett-TFRezw7pQwI-unsplash.jpg"
+import { Link } from "react-router-dom";
+import CondImg from "../assets/geoffrey-moffett-TFRezw7pQwI-unsplash.jpg";
 
-export const Card = ({movie}) => {
-  const {id, title, overview, poster_path}=movie
-  const image=poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : CondImg;
+export const Card = ({ movie }) => {
+  const { id, title, overview, poster_path } = movie;
+  const image = poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : CondImg;
+
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
       <Link to={`/movie/${id}`}>
-        <img className="rounded-t-lg" src={image} alt="" />
+        <img
+          className="rounded-t-lg"
+          src={image}
+          alt={title}
+          width="500"
+          height="750"
+        />
       </Link>
       <div className="p-5">
         <Link to={`/movie/${id}`}>
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-          
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
         </Link>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {overview}
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};

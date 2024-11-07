0a579c49f40b40fa428c7b9eb0ef55f7f3ce631d
favourachara07 <acharafavour01@gmail.com>
2024-11-07 17:28:01 +0100

    Changed MovieDetail interface

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,76 +1,62 @@
-/* eslint-disable react/prop-types */
-import { Link } from "react-router-dom"
-import { useParams } from "react-router-dom"
-import { useEffect, useState } from "react"
-import axios from "axios"
-import CondImg from "../assets/geoffrey-moffett-TFRezw7pQwI-unsplash.jpg"
-import { useTitle } from "../hooks"
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import CondImg from '../assets/geoffrey-moffett-TFRezw7pQwI-unsplash.jpg';
+import { useTitle } from '../hooks';
 
 const MovieDetail = () => {
-  const params = useParams()
-  console.log(params.id)
-  const [data, setData] = useState({})
+  const params = useParams();
+  const [data, setData] = useState({});
   const image = data.poster_path ? `https://image.tmdb.org/t/p/w500/${data.poster_path}` : CondImg;
-  // !image? CondImg:""
-  
-  useEffect(()=>{axios.get(`https://api.themoviedb.org/3/movie/${params.id}?api_key=e9fe41fd5626415eb9b028cc98a24587`)
-    .then(response => setData(response.data))
-    .catch(error => console.error("Error fetching movies:", error));},[params.id])
+  const backgroundImage = data.backdrop_path ? `https://image.tmdb.org/t/p/w1280/${data.backdrop_path}` : CondImg;
 
-    useTitle(data.title ? data.title : '')
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/movie/${params.id}?api_key=e9fe41fd5626415eb9b028cc98a24587`)
+      .then(response => setData(response.data))
+      .catch(error => console.error("Error fetching movies:", error));
+  }, [params.id]);
 
-  console.log(data)
-  // const image=poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : CondImg;
+  useTitle(data.title ? data.title : '');
+
   return (
-    <main>
-      <section className="flex justify-around flex-wrap py-5">
-        <div className="max-w-sm">
-          <Link>
-            <img className="rounded" src={image} alt="" />
-          </Link>
-        </div>
-        <div className="max-w-2xl text-gray-700 text-lg dark:text-white">
-          <h1 className="text-4xl font-bold my-3 text-center lg:text-left">{data.title}</h1>
-          <p className="my-4">{data.overview}</p>
-          <p className="my-7 flex-wrap gap-2">
-            {data.genres && data.genres.map((genre) => (
-              <span className="mr-2 border border-gray-200 rounded dark:border-gray-600 p-2" key={genre.id}>{genre.name}</span>
-            ))}
-          </p>
-
-
-          <div className="flex items-center">
-            <svg className="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <p className="ml-2 text-gray-900 dark:text-white">{data.vote_average}</p>
-            <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-            <span className="font-medium text-gray-900 hover:no-underline dark:text-white">{data.vote_count} reviews</span>
-          </div>
-          <p className=" my-4">
-            <span className="font-bold mr-4">Runtime:</span>
-            <span>{data.runtime} minutes</span>
-          </p>
-          <p className=" my-4">
-            <span className="font-bold mr-4">Budget:</span>
-            <span>{data.budget}</span>
-          </p>
-          <p className=" my-4">
-            <span className="font-bold mr-4">Runtime:</span>
-            <span>{data.revenue}</span>
-          </p>
-          <p className=" my-4">
-            <span className="font-bold mr-4">Release Date:</span>
-            <span>{data.release_date}</span>
-          </p>
-          <p className=" my-4">
-            <span className="font-bold mr-4">IMDB Code:</span>
-            <a href={`https://www.imdb.com/title/${data.imdb_id}`} target='_blank' rel="noreferrer">{data.imdb_id}</a>
-          </p>
+    <main className="max-w-7xl mx-auto min-h-[90vh] p-2.5">
+      <section className="relative w-full h-[500px]">
+        <img src={backgroundImage} alt={data.title} className="absolute inset-0 w-full h-full object-cover" width="1280" height="500" />
+        <div className="relative flex justify-center items-center h-full">
+          <img src={image} alt={data.title} className="w-64 h-auto" width="500" height="750" />
         </div>
       </section>
+      <section className="max-w-2xl mx-auto text-gray-700 text-lg dark:text-white">
+        <h1 className="text-3xl font-bold my-4">{data.title}</h1>
+        <p className="my-4">{data.overview}</p>
+        <div className="flex items-center my-4">
+          <p className="ml-2 text-gray-900 dark:text-white">{data.vote_average}</p>
+          <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+          <span className="font-medium text-gray-900 hover:no-underline dark:text-white">{data.vote_count} reviews</span>
+        </div>
+        <p className="my-4">
+          <span className="font-bold mr-4">Runtime:</span>
+          <span>{data.runtime} minutes</span>
+        </p>
+        <p className="my-4">
+          <span className="font-bold mr-4">Budget:</span>
+          <span>{data.budget}</span>
+        </p>
+        <p className="my-4">
+          <span className="font-bold mr-4">Revenue:</span>
+          <span>{data.revenue}</span>
+        </p>
+        <p className="my-4">
+          <span className="font-bold mr-4">Release Date:</span>
+          <span>{data.release_date}</span>
+        </p>
+        <p className="my-4">
+          <span className="font-bold mr-4">IMDB Code:</span>
+          <span>{data.imdb_id}</span>
+        </p>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default MovieDetail
+export default MovieDetail;
